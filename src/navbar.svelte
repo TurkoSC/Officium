6be64a739a38d1611f6178e6c9c96986512d71d6
TurkoSC <galicalomg@gmail.com>
2020-11-16 13:17:08 +0100

    NavBar(TopBar)
    NavBar sin sidebar.

---
+++ src/navbar.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Logo from './Logo.svelte'
+	import BotonSidebar from './BotonSidebar.svelte'
+	
+	export let sidebar = false
+</script>
+
+<header class="flex justify-between bg-gray-200 p-2 items-center text-gray-600 border-b-2">
+	<nav class="flex">
+		<BotonSidebar bind:open={sidebar}/>
+		<Logo/>	
+	</nav>
+	<header class="flex justify-between bg-gray-200 p-2 items-center text-gray-600 border-b-2">
+	<nav class="flex">
+		<BotonSidebar bind:open={sidebar}/>
+		<Logo/>	
+	</nav>
+	<Menu/>
+</header>
