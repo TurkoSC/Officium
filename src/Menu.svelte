1fc01271871ee2399bbbe4aa5bfdd5c272076d06
TurkoSC <galicalomg@gmail.com>
2020-11-16 13:19:44 +0100

    NavBar
    Pulir navbar

--- src/Menu.svelte
+++
@@ -1,4 +0,0 @@
-<nav class="hidden text-gray-500 uppercase text-bold sm:block">
-    <a href="/about" class="hover:text-gray-700 hover:no-underline">Cosas</a>
-	<a href="/contact" class="hover:text-gray-700 hover:no-underline">máscosas</a>
-  </nav>
